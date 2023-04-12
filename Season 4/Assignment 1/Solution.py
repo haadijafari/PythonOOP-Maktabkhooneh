bc02c9040de7cb57843be328df2a31e9ca43e06b
Hadi Jafari <sig.haadijafari@gmail.com>
2023-04-12 22:37:24 +0430

    flake8
    [ * ] edited code structure compatible with pep8 using flake8

--- Season 4/Assignment 1/Solution.py
+++ Season 4/Assignment 1/Solution.py
@@ -1,20 +1,22 @@
 class Person:
-    def __init__(self, name:str(), age:int()):
+    def __init__(self, name: str(), age: int()):
         self.name = name.title()
         self.age = age
-        
+
     def display(self):
         print(f'{self.name} -->\nAge: {self.age}')
-        
+
+
 class Student(Person):
-    def __init__(self, name:str(), age:int(), major:str()):
+    def __init__(self, name: str(), age: int(), major: str()):
         super().__init__(name, age)
         self.major = major.title()
-    
+
     def displayStudent(self):
         super().display()
         print(f'Major: {self.major}')
-        
+
+
 if __name__ == '__main__':
     stu1 = Student('hadi', 20, 'computer science')
     stu1.displayStudent()
