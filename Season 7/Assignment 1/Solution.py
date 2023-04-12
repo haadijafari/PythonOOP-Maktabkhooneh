85efefffe5b8f4a052d06a7757bdf95cebaead5e
Hadi Jafari <sig.haadijafari@gmail.com>
2023-04-13 00:04:25 +0430

    Season 7 - Assignment 1
    [+] Assignment description has been added ("assignmnet.png")
    [+] Python solution file has been added ("Solution.py")

---
+++ Season 7/Assignment 1/Solution.py
@@ -0,0 +1,42 @@
+from abc import ABC
+import random
+from string import ascii_letters, digits
+
+
+class Password(ABC):
+    def generate(self):
+        pass
+
+
+class PasswordChar(Password):
+    pass_length = 20
+
+    def generate(self):
+        q_list = [random.choice(ascii_letters)
+                  for i in range(self.pass_length)]
+        return ''.join(q_list)
+
+
+class PasswordInteger(Password):
+    pass_length = 20
+
+    def generate(self):
+        q_list = [random.choice(digits) for i in range(self.pass_length)]
+        return ''.join(q_list)
+
+
+class PasswordCombo(Password):
+    pass_length = 20
+
+    def generate(self):
+        q_list = [random.choice(ascii_letters + digits)
+                  for i in range(self.pass_length)]
+        return ''.join(q_list)
+
+
+if __name__ == '__main__':
+    p1 = PasswordChar()
+    p2 = PasswordInteger()
+    p3 = PasswordCombo()
+
+    print(p1.generate(), p2.generate(), p3.generate(), sep='\n')
